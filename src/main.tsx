1938d8bf44a973f70bac9e8e326b71b11d999888
findwannawhy <findwannawhy@gmail.com>
2025-11-10 16:07:06 +0300

    feat: add PWA support and SSL configuration
    - Integrated PWA functionality using vite-plugin-pwa, including a manifest and icons for different sizes.
    - Configured HTTPS support in Vite with self-signed certificates.
    - Updated index.html to include PWA meta tags and icons.
    - Added service worker registration in main.tsx for offline capabilities.
    - Included new certificate and key files for local development.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './APP.tsx'
 import { Provider } from "react-redux"
 import store from "./store"
+import {registerSW} from "virtual:pwa-register"
 
+if ("serviceWorker" in navigator) {
+  registerSW()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import fs from 'fs';
+import path from 'path';
 
 // Определяем, работаем ли мы в Docker (проверяем переменную окружения из docker-compose.yml)
 const isDocker = process.env.DOCKER === 'true';
-const apiHost = isDocker ? 'http://host.docker.internal:8080' : 'http://localhost:8080';
+//const apiHost = isDocker ? 'http://host.docker.internal:8080' : 'http://localhost:8080';
 const minioHost = isDocker ? 'http://host.docker.internal:9000' : 'http://localhost:9000';
 
 export default defineConfig({
   server: {
+    https:{
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
     proxy: {
       "/api": {
-        target: apiHost,
+        target: "http://192.168.1.4:8080",
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, "/"),
       },
       "/img": {
         target: minioHost,
@@ -26,6 +33,42 @@ export default defineConfig({
     host: true, // для правильного маппинга портов в Docker
     strictPort: true, // не даст использовать другой порт если 3000 занят
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Helix",
+        short_name: "Helix",
+        start_url: "/IAD-Semester5-UI/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#171A20",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "flask-icon-180.png",
+            type: "image/png",
+            sizes: "180x180",
+            purpose: "any maskable"
+          },
+          {
+            src: "flask-icon-512.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: "flask-icon-192.png",
+            type: "image/png",
+            sizes: "192x192"
+          }
+        ],
+      }
+    }),
+  ],
   base: "/IAD-Semester5-UI/",
 })
