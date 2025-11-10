9daf98ff576a9f2b75e068fbd9cc47077e67c8a9
findwannawhy <findwannawhy@gmail.com>
2025-11-10 16:31:16 +0300

    style: update Header, SampleCard, and SamplesList for improved responsiveness
    - Modified CSS for Header to enhance layout and navigation responsiveness on various screen sizes.
    - Adjusted SampleCard styles to ensure better adaptability for mobile devices.
    - Updated SamplesList styles to support flexible layouts and improved alignment for larger mobile screens.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mtljqg43lsg"
+    "revision": "0.a2oolvd8jag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
